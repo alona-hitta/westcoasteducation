76210cacd920e074c0b53d127128a31a931c2202
Alona <alona.kozhushchenko@hitta.se>
2023-01-23 10:56:08 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+
+
+import { screen, render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Routing', () => {
+  const setup = () => render(<App />);
+
+  test('Navigation and rendering correct component', async () => {
+    setup();
+
+    expect(screen.getByText(/Welcome to Westcoast Education/i)).toBeInTheDocument();
+    expect(screen.getByText(/Here you can find lists of teachers, lists of courses, add teachers or courses/i)).toBeInTheDocument();
+
+  });
 });
